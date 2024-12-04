0ca569d0d4cba69badb5ec02e4647bcf7d6121fd
Wai Yi <wongwaverley@gmail.com>
2024-12-04 00:29:21 +0000

    change debug to true

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8&f^+i_j-ci7+azzg(mcyffy)&bt7zy%wt4msh8au&r4wg7s-s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-waiyiwong-djangoproject-9iz70lnkwu4.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
